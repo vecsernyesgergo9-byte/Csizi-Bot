6154315634c69029dbfb9fd1e7ba8444bb32ae50
vecsernyesgergo9-byte <vecsernyesgergo9@gmail.com>
2026-09-18 22:20:53 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -242,8 +242,16 @@ class OtletGombok(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
-       @discord.ui.button(label="Elfogadás", style=discord.ButtonStyle.success, emoji="✅")
-    async def elfogadas(self, interaction: discord.Interaction, button: discord.ui.Button):
+    @discord.ui.button(
+        label="Elfogadás",
+        style=discord.ButtonStyle.success,
+        emoji="✅"
+    )
+    async def elfogadas(
+        self,
+        interaction: discord.Interaction,
+        button: discord.ui.Button
+    ):
         staff_role = interaction.guild.get_role(1449738506741088306)
 
         if staff_role not in interaction.user.roles:
@@ -253,9 +261,9 @@ class OtletGombok(discord.ui.View):
             )
             return
 
-         embed = interaction.message.embeds[0]
+        embed = interaction.message.embeds[0]
 
-         embed.add_field(
+        embed.add_field(
             name="Állapot",
             value=f"✅ Elfogadva – {interaction.user.mention}",
             inline=False
@@ -264,11 +272,22 @@ class OtletGombok(discord.ui.View):
         for item in self.children:
             item.disabled = True
 
-         await interaction.response.edit_message(embed=embed, view=self)
-     @discord.ui.button(label="Elutasítás", style=discord.ButtonStyle.danger, emoji="❌")
-    async def elutasitas(self, interaction: discord.Interaction, button: discord.ui.Button):
-        
-            staff_role = interaction.guild.get_role(1449738506741088306)
+        await interaction.response.edit_message(
+            embed=embed,
+            view=self
+        )
+
+    @discord.ui.button(
+        label="Elutasítás",
+        style=discord.ButtonStyle.danger,
+        emoji="❌"
+    )
+    async def elutasitas(
+        self,
+        interaction: discord.Interaction,
+        button: discord.ui.Button
+    ):
+        staff_role = interaction.guild.get_role(1449738506741088306)
 
         if staff_role not in interaction.user.roles:
             await interaction.response.send_message(
@@ -288,7 +307,10 @@ class OtletGombok(discord.ui.View):
         for item in self.children:
             item.disabled = True
 
-        await interaction.response.edit_message(embed=embed, view=self)
+        await interaction.response.edit_message(
+            embed=embed,
+            view=self
+        )
 @bot.tree.command(name="otlet", description="Küldj be egy ötletet a szerverhez.")
 @discord.app_commands.guilds(discord.Object(id=1449737996436766802))
 async def otlet(interaction: discord.Interaction, otlet: str):
